43cf1ab8a3528e43ffb8d8f3759466e130c44b0b
valeriawabwire <162123910+valeriawabwire@users.noreply.github.com>
2024-04-27 16:57:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+const feeEndpoint = 'http://localhost:3000/FeeDetails';
+console.log(feeEndpoint);
+
+document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementsByClassName("btn");
     Array.from(searchButton).forEach(button => {
-       button.addEventListener('click', getStudentDetails);
-   });
-
-    function getStudentDetails() {
-        fetch(`http://localhost:3001/FeeDetails`)
+               button.addEventListener('click', getStudentDetails);
+             });
+             function getStudentDetails(){
+    fetch(feeEndpoint)
         .then(response => response.json())
         .then(data => {
-            const { Results } = data;
-            console.log(Results);
+           
+            console.log(data);
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
+            console.error('Error fetching fee data: ', error);
         });
     }
-    
-});
-document.getElementById("show-announcements-btn").addEventListener("click", function() {
-    var announcementsText = "Consequences of Late Payment of Fees\n\n" +
-        "If fees are not paid by the specified deadline, the following consequences may occur:\n\n" +
-        "- Penalties or late fees may be imposed.\n" +
-        "- Access to certain campus facilities or services may be restricted.\n" +
-        "- Registration for future classes or academic activities may be blocked.\n" +
-        "- Transcripts or academic records may be withheld.\n\n" +
-        "It is important to ensure timely payment of fees to avoid these consequences.";
-
-    // Display the announcements as an alert
-    alert(announcementsText);
-});
+    })
