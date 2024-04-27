54bca727831c2311aafba6d0ce120827537cf18a
geraldkisilu <muligerald6@gmail.com>
2024-04-27 17:33:39 +0300

    commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ console.log(feeEndpoint);
 
 document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementsByClassName("btn");
-    Array.from(searchButton).forEach(button => {
-               button.addEventListener('click', getStudentDetails);
-             });
-             function getStudentDetails(){
+    // Array.from(searchButton).forEach(button => {
+    //            button.addEventListener('click', getStudentDetails);
+    //          });
+            //  function getStudentDetails(){
     fetch(feeEndpoint)
         .then(response => response.json())
         .then(data => {
@@ -16,5 +16,5 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch(error => {
             console.error('Error fetching fee data: ', error);
         });
-    }
+    // }
     })
