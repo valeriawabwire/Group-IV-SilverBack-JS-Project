1df8dbf3ebb154526cee83e21a2ba4d0c4229a30
valeria wabwire <larriebliss@gmail.com>
2024-04-27 16:49:04 +0300

    updatedjs

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function () {
+//     const searchButton = document.getElementsByClassName("btn");
+//     Array.from(searchButton).forEach(button => {
+//         button.addEventListener('click', getStudentDetails);
+//     });
+
+//     function getStudentDetails() {
+//         fetch(`http://localhost:3001/FeeDetails`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 const { Results } = data;
+//                 console.log(Results);
+//             })
+//             .catch(error => {
+//                 console.error('Error fetching data:', error);
+//             });
+//     }
+
+//});
+const feeEndpoint = 'http://localhost:3000/FeeDetails';
+console.log(feeEndpoint);
+
+document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementsByClassName("btn");
     Array.from(searchButton).forEach(button => {
-       button.addEventListener('click', getStudentDetails);
-   });
-
-    function getStudentDetails() {
-        fetch(`http://localhost:3001/FeeDetails`)
+               button.addEventListener('click', getStudentDetails);
+             });
+             function getStudentDetails(){
+    fetch(feeEndpoint)
         .then(response => response.json())
         .then(data => {
-            const { Results } = data;
-            console.log(Results);
+           
+            console.log(data);
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
+            console.error('Error fetching fee data: ', error);
         });
     }
-    
-});
+    })
