986ae2d24b01110175b2b730664c4edf068a5cf6
valeria wabwire <larriebliss@gmail.com>
2024-04-27 10:57:43 +0300

    fetch

--- index.js
+++ index.js
@@ -1,51 +1,19 @@
-ocument.addEventListener('DOMContentLoaded', function() {
-const search = document.getElementsByClassName("btn");
+Document.addEventListener('DOMContentLoaded', function() {
+    const searchButton = document.getElementsByClassName("btn");
+    Array.from(searchButton).forEach(button => {
+       button.addEventListener('click', getStudentDetails);
+   });
 
-searchBtn.addEventListener('click', getMealList);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}  
-);
+    function getStudentDetails() {
+        fetch(`http://localhost:3001/FeeDetails`)
+        .then(response => response.json())
+        .then(data => {
+            const { Results } = data;
+            console.log(Results);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    }
+    searchButton.addEventListener('onclick', getStudentDetails);
+});
