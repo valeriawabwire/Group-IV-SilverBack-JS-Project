7e3ac6818f1de029edf45a1851a38c499cf8835c
valeria wabwire <larriebliss@gmail.com>
2024-04-30 08:38:48 +0300

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const searchButton = document.getElementsByClassName("btn");
-//     Array.from(searchButton).forEach(button => {
-//         button.addEventListener('click', getStudentDetails);
-//     });
-
-//     function getStudentDetails() {
-//         fetch(`http://localhost:3001/FeeDetails`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 const { Results } = data;
-//                 console.log(Results);
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching data:', error);
-//             });
-//     }
-
-//});
 const feeEndpoint = 'http://localhost:3000/FeeDetails';
 console.log(feeEndpoint);
 
